aefc84e609fab09dc230fdcb13a32dbf5d88126f
RAKITEMBO <lewalex18@gmail.com>
2022-12-19 01:42:33 +0300

    Ошибка при выводе book

--- lesson21/src/main/java/com/tms/CarsServlet.java
+++ lesson21/src/main/java/com/tms/CarsServlet.java
@@ -1,7 +1,7 @@
 package com.tms;
 
 
-import javax.servlet.ServletOutputStream;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,11 +10,29 @@ import java.io.IOException;
 import java.rmi.ServerException;
 
 public class CarsServlet extends HttpServlet {
+
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServerException, IOException {
+        System.out.println("-------- DO GET --------");
         ServletOutputStream outputStream = resp.getOutputStream();
-        outputStream.println("Hello my first servlet");
+        outputStream.println("Hello my first servlet!!!");
         outputStream.close();
 
+
+
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("-------- DO INIT --------");
+        super.init(config);
+    }
+
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
     }
 }
