3220c369c79f9e93bfce6441f28b58b44faa3bff
RAKITEMBO <lewalex18@gmail.com>
2022-12-22 17:18:39 +0300

    21 finish

--- lesson21/src/main/java/com/tms/BookServlet.java
+++ lesson21/src/main/java/com/tms/BookServlet.java
@@ -6,14 +6,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookServlet extends HttpServlet {
 
+
+    private static Map<String, String> books = new HashMap<>();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/book.jsp").forward(req, resp);
+        String id = req.getParameter("id");
+
+        resp.getOutputStream().print(books.get(id));
 
 
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String title = req.getParameter("title");
+        books.put(id, title);
+    }
+
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        books.remove(id);
+    }
 }
